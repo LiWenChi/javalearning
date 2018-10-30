f25510c6f4aba586b6898c6179bf92ed8cd5a3a3
liwenchi <liwenchi@classba.cn>
2018-10-30 15:21:31 +0800

    测试01

---
+++ src/main/java/test.java
@@ -0,0 +1,10 @@
+import sun.applet.Main;
+
+/**
+ * Created by Administrator on 2018/10/30.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+}
